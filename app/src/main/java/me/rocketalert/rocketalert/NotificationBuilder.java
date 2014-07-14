11dce184a1f4db8bb5a4ef0d52f49cb3ae27fa3f
Arik Fraimovich <arik@arikfr.com>
2014-07-14 21:43:54 +0300

    Support for selecting different alert sound

--- app/src/main/java/me/rocketalert/rocketalert/NotificationBuilder.java
+++ app/src/main/java/me/rocketalert/rocketalert/NotificationBuilder.java
@@ -8,19 +8,28 @@ import android.media.RingtoneManager;
 import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 
 public class NotificationBuilder {
+    private static final String TAG = "me.rocketalert.NotificationBuilder";
+
     public static Notification getNotification(Context context, long timestamp, String areaName,
                                                String locations, int timeToCover) {
         // TODO: proper timestamp
         // countdown? http://developer.android.com/training/notify-user/display-progress.html
         // TODO: localization
         String title = String.format("%s (%d שניות)", areaName, timeToCover);
+
         Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        String tickerText = String.format("%s: %s", areaName, locations);
-
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        String sound = prefs.getString("notificationSound", null);
 
+        if (sound != null) {
+            Log.d(TAG, "Using sound from: " + sound);
+            alarmSound = Uri.parse(sound);
+        }
+
+        String tickerText = String.format("%s: %s", areaName, locations);
 
         NotificationCompat.Builder builder =
                 new NotificationCompat.Builder(context)
